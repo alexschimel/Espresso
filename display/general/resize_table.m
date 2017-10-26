ab830c104317310fbc965e8247e18defbda41cd9
Yoann Ladroit <yoann.ladroit@niwa.co.nz>
2017-10-27 08:51:03 +1300

    -minor fix

--- display/general/resize_table.m
+++ display/general/resize_table.m
@@ -6,7 +6,7 @@ if~isempty(table)&&isvalid(table)
     pos_f=getpixelposition(src);
     width_t_old=nansum([column_width{:}]);
     width_t_new=pos_f(3);
-    new_width=cellfun(@(x) x/width_t_old*width_t_new,column_width,'un',0);
+    new_width=cellfun(@(x) x/width_t_old*width_t_new,column_width,'uniformoutput',0);
     set(table,'ColumnWidth',new_width);
 end
 end
