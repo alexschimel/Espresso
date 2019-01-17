bbeacd39b44de98dcb33e588cf3956dab0d4813d
yladroit <yoann.ladroit@niwa.co.nz>
2019-01-17 16:55:05 +1300

    -updates color on stacked wc

--- display/listeners/listenAct_features.m
+++ display/listeners/listenAct_features.m
@@ -79,36 +79,39 @@
 function listenAct_features(~,~,main_figure)
 
 disp_config = getappdata(main_figure,'disp_config');
+stacked_wc_tab_comp  = getappdata(main_figure,'stacked_wc_tab');
 
 map_tab_comp = getappdata(main_figure,'Map_tab');
-
-features_h     = findobj(map_tab_comp.map_axes,{'tag','feature'});
-features_text_h = findobj(map_tab_comp.map_axes,{'tag','feature_text'});
-
-if isempty(features_h)
-    return;
-end
-
-% colours: first for inactive, second for active
-col = {[0.1 0.1 0.1],'r'};
-
-for ii = 1:numel(features_h)
+ah_tot = [map_tab_comp.map_axes stacked_wc_tab_comp.wc_axes];
+for iax=1:numel(ah_tot)
+    ax=ah_tot(iax);
+    features_h     = findobj(ax,{'tag','feature'});
+    features_text_h = findobj(ax,{'tag','feature_text'});
     
-    % feature
-    isAct = ismember(features_h(ii).UserData,disp_config.Act_features);
-    switch features_h(ii).Type  
-        case 'line'
-            features_h(ii).Color = col{isAct+1};
-            features_h(ii).MarkerFaceColor = col{isAct+1};
-        case 'polygon'
-            features_h(ii).EdgeColor = col{isAct+1};
-            features_h(ii).FaceColor = col{isAct+1};
+    if isempty(features_h)
+        return;
     end
     
-    % text
-    isAct = ismember(features_text_h(ii).UserData,disp_config.Act_features);
-    features_text_h(ii).Color = col{isAct+1};
+    % colours: first for inactive, second for active
+    col = {[0.1 0.1 0.1],'r'};
     
+    for ii = 1:numel(features_h)
+        
+        % feature
+        isAct = ismember(features_h(ii).UserData,disp_config.Act_features);
+        switch features_h(ii).Type
+            case 'line'
+                features_h(ii).Color = col{isAct+1};
+                features_h(ii).MarkerFaceColor = col{isAct+1};
+            case 'polygon'
+                features_h(ii).EdgeColor = col{isAct+1};
+                features_h(ii).FaceColor = col{isAct+1};
+        end
+        
+        % text
+        isAct = ismember(features_text_h(ii).UserData,disp_config.Act_features);
+        features_text_h(ii).Color = col{isAct+1};
+        
+    end
 end
-
 end
